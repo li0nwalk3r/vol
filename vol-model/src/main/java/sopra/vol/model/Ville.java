254e2dc6b577e45f7375bb6e1b20d18c1c2b9dd5
AntoineCdm <antoine.cdm@orange.fr>
2019-03-08 14:09:08 +0100

    Aeroport ville escale jpa labelling

--- vol-model/src/main/java/sopra/vol/model/Ville.java
+++ vol-model/src/main/java/sopra/vol/model/Ville.java
@@ -3,9 +3,23 @@ package sopra.vol.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.persistence.Version;
+
+@Entity
 public class Ville {
+	@Id
+	@GeneratedValue
 	private Long id;
+	@Version
+	private int version;
+	@Column(name="nom", length=100)
 	private String nom;
+	@Transient
 	private List<Aeroport> aeroports = new ArrayList<>();
 
 	public Ville() {
