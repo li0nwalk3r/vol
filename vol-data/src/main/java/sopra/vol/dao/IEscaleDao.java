abb79d880bd9c4b6643885460d4d2f38f212c673
AntoineCdm <antoine.cdm@orange.fr>
2019-03-08 15:42:30 +0100

    Push EscaleDaoJpa et  IEscaleDao

---
+++ vol-data/src/main/java/sopra/vol/dao/IEscaleDao.java
@@ -0,0 +1,7 @@
+package sopra.vol.dao;
+
+import sopra.vol.model.Escale;
+
+public interface IEscaleDao extends IDao<Escale, Long>{
+
+}
---
+++ vol-data/src/main/java/sopra/vol/dao/jpa/EscaleDaoJpa.java
@@ -0,0 +1,34 @@
+package sopra.vol.dao.jpa;
+
+import java.util.List;
+
+import sopra.vol.dao.IEscaleDao;
+import sopra.vol.model.Escale;
+
+public class EscaleDaoJpa implements IEscaleDao {
+
+	@Override
+	public List<Escale> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Escale find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Escale save(Escale obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Escale obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
